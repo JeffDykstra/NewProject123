f0e418b4b428e5b937c5349fe38b9ed43fe8188e
Jeff Dykstra <Jeff.Dykstra@mcirosoft.com>
2026-02-11 08:39:58 -0600

    add function

--- test.ps1
+++ test.ps1
@@ -1,2 +1,21 @@
 'hello world2'
 'just another line'
+
+function Verb-Noun {
+    [CmdletBinding()]
+    param (
+        
+    )
+    
+    begin {
+        
+    }
+    
+    process {
+        
+    }
+    
+    end {
+        
+    }
+}
